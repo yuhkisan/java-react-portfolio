662fcd1f30bbb71a2428cdf64c06e4f848a3b8c4
yuki <yukimurasawa0903@gmail.com>
2026-09-23 19:26:09 +0900

    feat: Spring Bootの土台とDB設定を追加する

---
+++ backend/src/test/java/com/yuhkisan/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhkisan.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
